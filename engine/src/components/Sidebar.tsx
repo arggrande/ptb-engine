14d967cc9ff531fa883f8f8f3b7c86a4784f5189
Adam Grande <adam.grande@outlook.com>
2020-08-18 07:54:12 +0800

    Added link and styling

--- engine/src/components/Sidebar.tsx
+++ engine/src/components/Sidebar.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import './Sidebar.css'
 
-export default function Sidebar(sidebarProps: SidebarProps) {
+export default function Sidebar(props: SidebarProps) {
 
     return (
         <div className='container'>
-            <img className='logo' src={sidebarProps.avatarUri} alt='Avatar' />
+            <img className='logo' src={props.avatarUri} alt='Avatar' />
             <div className='titleBlock'>
-                <span className='title'>{sidebarProps.title}</span>
-                <span className='subText'>{sidebarProps.subText}</span>
+                <span className='title'>{props.title}</span>
+                <span className='subText'>{props.subText}</span>
             </div>
             <div className='navigation'>
                 <span className='navlink'>Home</span>
                 <span className='navlink'>About</span>
             </div>
             <div className='twitter'>
-                <span className="fab fa-twitter fa-sm icon" ></span>
+                <a href={props.twitterBioUri} target='_blank' style={{textDecoration: 'none'}}><span className="fab fa-twitter fa-sm icon" ></span></a>
             </div>
         </div>
     );

d8f7f844d3d5af3b550eb07393aa9f2ee19fd04a
Adam Grande <adam.grande@outlook.com>
2020-08-21 18:23:20 +0800

    Finished main page

--- engine/src/components/PostItem.tsx
+++ engine/src/components/PostItem.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import './PostItem.css'
 import moment from 'moment';
+import ClampLines from  'react-clamp-lines';
+
 
 export default function PostItem(props: PostItemProps) {
 
+
   return (
+    
     <article className='postItemContainer'>
-      <header>
+      <header className='postTitle'>
         <time className='postDate'>{moment(props.date).format("MMMM d, YYYY")}</time>
         <h2>{props.title}</h2>
       </header>
       <div className='postBody'>
-        <p className='postBody'>{props.body}</p>
+        <ClampLines text={props.body} id={'clamp'} className='postBody' ellipsis='...' lines={3} innerElement={'p'} buttons={false} />
       </div>
     </article>
   )

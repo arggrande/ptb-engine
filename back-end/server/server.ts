765c1802be1bc69ceef1dba8a3891f6dc50d8fd6
Adam Grande <adam.grande@outlook.com>
2020-08-30 11:25:10 +0800

    Got webpack and HMR working

--- back-end/server/server.ts
+++ back-end/server/server.ts
@@ -24,11 +24,34 @@ app.use(Express.json());
 //  - debugging ability
 
 app.get('/', function(request, response) {
-  response.send('hello... is it me youre looking for?')
+  response.send('hello... is it me youre looking for? yes it is.')
 });
 
 app.use(Posts);
 
-app.listen(PORT, function() {
+const server = app.listen(PORT, function() {
   console.log(`listening... on port ${PORT}`);
 });
+
+type ModuleId = string | number;
+
+interface WebpackHotModule {
+  hot? : {
+    data: any,
+    accept(
+      dependencies: string[],
+      callback?: (updatedDependencies: ModuleId[]) => void
+    ): void,
+    accept(dependency: string, callback?: () => void): void,
+    accept(errHandler?: (err: Error) => void): void,
+    dispose(callback: (data: any) => void): void
+    
+  };
+}
+
+declare const module: WebpackHotModule;
+
+if(module.hot) {
+  module.hot.accept();
+  module.hot.dispose(() => server.close());
+}

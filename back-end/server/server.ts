c3bfe2264e9e884d5d289c8368b63b6ae32dcf58
Adam Grande <adam.grande@outlook.com>
2020-08-30 10:35:32 +0800

    Added todos

--- back-end/server/server.ts
+++ back-end/server/server.ts
@@ -3,6 +3,11 @@ import Posts from './routes/posts';
 
 const app: Express.Application = Express();
 
+// todo:
+//  - auth middleware
+//  - logging middleware
+//  - debugging ability
+
 app.get('/', function(request, response) {
   response.send('hello... is it me youre looking for?')
 });

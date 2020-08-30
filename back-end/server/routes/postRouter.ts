f6749f59f8f7ab199b6a794dc91773c2282e70e9
Adam Grande <adam.grande@outlook.com>
2020-08-31 06:52:31 +0800

    Added some nicer status codes and error handling

--- back-end/server/routes/postRouter.ts
+++ back-end/server/routes/postRouter.ts
@@ -1,29 +1,36 @@
 import Express from 'express';
 import { getAllPosts, addPost, getPostByTitle } from '../services'
-
+import { PostModel } from '../models';
 
 let router = Express.Router();
 
 router.get('/posts', async function(req, res) {
+
   let posts = await getAllPosts();
-
-
-  res.send(posts);
+  res.status(200).send(posts);
 });
 
 router.get('/posts/:title', async function(req, res) {
-  console.log(`getting title: ${req.params.title}`);
+
   const post = await getPostByTitle(req.params.title);
-  res.send(post);
+  console.log(post);
+  if(post === undefined) {
+    res.status(404).send('not found');
+  }
+
+  res.status(200).send(post);
 });
 
 router.post('/posts', async function(req, response) {
-  console.log('posted to posts. ha.');
-  
-  // can i strongly type req.body?
-  console.log(req.body);
-  
-  // todo: look into how i progress the response from here?
+
+  try {
+    const post: PostModel = req.body.post;
+    await addPost(post);
+    response.status(201);
+  }
+  catch (e) {
+    response.status(404).send(e.message);
+  }
 });
 
 export default router;
